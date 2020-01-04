d6ac50d8c73d35dbe6d6f8e91e0bc5583ee5a240
vj201292 <vj.ku.99@gmail.com>
2020-01-04 21:06:31 +0530

    first commit

---
+++ hello-world/src/main/java/com/vj/kubernetes/HelloController.java
@@ -0,0 +1,13 @@
+package com.vj.kubernetes;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/hello")
+    public String getHello(){
+        return "welcome to kubernetes";
+    }
+}
